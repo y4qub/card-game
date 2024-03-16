20d4a90f86f8a10455cf0ed02743ffc259f65851
Yaqub <murcja812@gmail.com>
2024-03-16 21:32:34 +0100

    add sample cards

---
+++ helpers/cards.ts
@@ -0,0 +1,22 @@
+import { Card } from '@/interfaces/card'
+
+export const cards: Card[] = [
+  {
+    name: 'Kacer Donald',
+    attack: 3,
+    health: 5,
+    description: 'Draw 7 cards.',
+  },
+  {
+    name: 'Deathwing',
+    attack: 8,
+    health: 12,
+    description: 'Destroy everything.',
+  },
+  {
+    name: 'Alivewing',
+    attack: 1,
+    health: 3,
+    description: 'Kills every minion.',
+  },
+]
